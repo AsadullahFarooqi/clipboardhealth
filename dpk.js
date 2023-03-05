3ca8fe4f3606a7ce2474c6f69ea56d96ae270487
Asadullah Farooqi <asadullah.itcgcs@gmail.com>
2023-03-05 12:27:42 +0500

    refactor dpkg

---
+++ dpk.js
@@ -0,0 +1,35 @@
+const crypto = require("crypto");
+
+// constants
+const TRIVIAL_PARTITION_KEY = "0";
+const MAX_PARTITION_KEY_LENGTH = 256;
+
+// add hash function to convert event to partition key
+function createHash(event) {
+  const data = JSON.stringify(event);
+  res = crypto.createHash("sha3-512").update(data).digest("hex")
+  return res;
+}
+
+
+exports.deterministicPartitionKey = (event) => {
+  let candidate;
+  if (event && event.partitionKey) {
+    candidate = event.partitionKey;
+  } else if (event && !event.data.partitionKey) {
+    const data = JSON.stringify(event);
+    candidate = createHash(data);
+  }
+
+  if (candidate && typeof candidate !== "string") {
+    candidate = JSON.stringify(candidate);
+  } else {
+    candidate = TRIVIAL_PARTITION_KEY;
+  }
+
+  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+    candidate = createHash(candidate);
+  }
+  
+  return candidate;
+};
